45c748575435326d48e681d68881f1238e00cee7
WON-DER <58491427+WON-DER@users.noreply.github.com>
2021-01-04 22:28:38 -0800

    Added support
    Added support for Numpad functionality as well as Escape key for clearing display.

--- Calculator/js/main.js
+++ Calculator/js/main.js
@@ -3,15 +3,42 @@ let hudDisplay = document.getElementById("hudDisplay");
 let equals = document.getElementById("equals");
 let clear = document.getElementById("clear");
 let del = document.getElementById("delete");
-
+numPadKeys = [];
 equals.addEventListener("click", calculateInput);
 clear.addEventListener("click", clearHud);
 del.addEventListener("click", deleteCharacter);
 
+function getNumCodes() {
+  for (i = 96; i < 112; i++) {
+    numPadKeys.push(i);
+  }
+  for (i = 42; i < 58; i++) {
+    numPadKeys.push(i);
+  }
+  return numPadKeys;
+}
+
+getNumCodes();
+
+window.onkeydown = function (event) {
+  if (numPadKeys.indexOf(event.keyCode) !== -1) {
+    hudDisplay.value += event.key;
+  } else if (event.keyCode === 8) {
+    deleteCharacter();
+  } else if (event.keyCode === 13) {
+    calculateInput();
+  }
+};
+
+window.onkeyup = function (event) {
+  if (event.keyCode === 27) {
+    clearHud();
+  }
+};
+
 for (let i = 0; i < numKeys.length; i++) {
   numKeys[i].addEventListener("click", grabInput);
   function grabInput() {
-    // let result = numKeys[i].innerText;
     let result = numKeys[i].getAttribute("id");
     console.log(numKeys[i].innerText);
     hudDisplay.value += result;
